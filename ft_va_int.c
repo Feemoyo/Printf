7c2741af8e2796f23c54937988181ea3678ae2e6
user42 <lipemoreira.yorgos@gmail.com>
2021-09-05 20:56:27 -0300

    correção do %p

--- ft_va_int.c
+++ ft_va_int.c
@@ -6,7 +6,7 @@
 /*   By: fmoreira <fmoreira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 15:58:19 by fmoreira          #+#    #+#             */
-/*   Updated: 2021/09/05 20:39:12 by fmoreira         ###   ########.fr       */
+/*   Updated: 2021/09/05 20:54:46 by fmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@ void	master_hex(va_list args, char masks, int *count)
 	}
 	else if (masks == 112)
 	{
-		ft_putstr_fd("(nil)", 1);
+		ft_putstr_fd("0x0", 1);
 		(*count) += 5;
 	}
 	else
